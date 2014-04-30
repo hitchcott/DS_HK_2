72e1ab88943565623784726f34d8bd2207276d85
Chris Hitchcott <hitchcott@gmail.com>
2014-04-30 19:58:49 +0800

    First attempt at lesson 1

---
+++ code/student/lesson01/classwork.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python
+# Import required libs
+import sys
+
+# sart a counter and store the file in memory
+
+impressions = 0
+lines = sys.stdin.readlines()
+lines.pop(0)
+
+# find the sum of index 2 in the list
+
+oldest = 0
+clicks = 0
+ages = 0
+
+for line in lines:
+  split_line = line.strip().split(',')
+  if int(split_line[0]) > oldest:
+    oldest = int(split_line[0])
+  impressions = impressions + int(split_line[2])
+  clicks = clicks + int(split_line[3])
+  ages = ages + int(split_line[0])
+
+average_age = ages / len(lines)
+
+click_through_rate = float(clicks) / float(impressions)
+
+print 'Clicks', clicks
+print 'Impressions', impressions
+print 'Average Ages', average_age
+print 'Oldest', oldest
+print 'Click thorugh', click_through_rate
