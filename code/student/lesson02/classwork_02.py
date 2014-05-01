8fd8303460bfeb86091029f72291bf4157f91684
Chris Hitchcott <hitchcott@gmail.com>
2014-05-01 19:51:08 +0800

    Completed programmer work part 1

--- code/student/lesson02/classwork_02.py
+++ code/student/lesson02/classwork_02.py
@@ -4,16 +4,32 @@ matrix = [
   [2,4,6,8]
 ]
 
+
+
 print 'vector', vector
 print 'matrix', matrix
 
 def matrix_multiplication(matrix, multiplier):
   return [[cell * multiplier for cell in row] for row in matrix]
 
-print 'matrix_multiplication(matrix, 5)', matrix_multiplication(matrix, 5)
+print '\nmatrix_multiplication(matrix, 5)\n', matrix_multiplication(matrix, 5)
 
 def vectorMatrix_multiplication(matrix, vector):
   # I attmeped to do it using the inline style above, but was having issues
+  try:
+    for i in range(len(matrix)):
+      for j in range(len(vector)):
+        matrix[i][j] = vector[j] * matrix[i][j]
+      matrix[i] = sum(matrix[i])
+    return matrix
+  except:
+    print 'vectorMatrix_multiplication calculation eror.'
+
+print '\nvectorMatrix_multiplication(matrix, vector)\n', vectorMatrix_multiplication(matrix, vector)
+
+# use unittest eventually...
+print '\nTrying to multiply with bad vector [2,3]'
+print vectorMatrix_multiplication(matrix, [2,3])
 
   for i in range(len(matrix)):
     for j in range(len(vector)):
