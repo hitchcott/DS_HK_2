29f124f81f5a00c4c3148edab39261055e7a2c8a
Chris Hitchcott <hitchcott@gmail.com>
2014-05-01 19:31:03 +0800

    Solved second part naively, but it makes sense to me...

--- code/student/lesson02/classwork_02.py
+++ code/student/lesson02/classwork_02.py
@@ -11,3 +11,14 @@ def matrix_multiplication(matrix, multiplier):
   return [[cell * multiplier for cell in row] for row in matrix]
 
 print 'matrix_multiplication(matrix, 5)', matrix_multiplication(matrix, 5)
+
+def vectorMatrix_multiplication(matrix, vector):
+  # I attmeped to do it using the inline style above, but was having issues
+
+  for i in range(len(matrix)):
+    for j in range(len(vector)):
+      matrix[i][j] = vector[j] * matrix[i][j]
+    matrix[i] = sum(matrix[i])
+  return matrix
+
+print vectorMatrix_multiplication(matrix, vector)
